361e883409eec766b8d4acc8d42268b60b53303c
Cassandra Culkins <cassy.culkins@gmail.com>
2017-02-02 20:37:35 -0800

    add participation page copy and scss files

--- app/routes/news-media.js
+++ app/routes/news-media.js
@@ -2,3 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 });
+
+
+// $(document).ready(function(){
+//     $("#hide").click(function(){
+//         $("p").hide();
+//     });
+//     $("#show").click(function(){
+//         $("p").show();
+//     });
+// });
